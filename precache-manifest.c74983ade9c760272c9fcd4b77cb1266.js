312545adee6a3831af21deb35d7553ace79796d4
adnenre <adnen.rebai@gmail.com>
2018-11-29 17:55:00 +0100

    Updates

---
+++ precache-manifest.c74983ade9c760272c9fcd4b77cb1266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/manage-task/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7f75bf53f031e4995b7",
+    "url": "/manage-task/static/js/runtime~main.d7f75bf5.js"
+  },
+  {
+    "revision": "5d51ac67cdd8799d4794",
+    "url": "/manage-task/static/js/main.5d51ac67.chunk.js"
+  },
+  {
+    "revision": "bdc45e9b8bb0464a27b6",
+    "url": "/manage-task/static/js/1.bdc45e9b.chunk.js"
+  },
+  {
+    "revision": "5d51ac67cdd8799d4794",
+    "url": "/manage-task/static/css/main.14330b8a.chunk.css"
+  },
+  {
+    "revision": "0b4aa986dc32092274eac0f3d9e6561a",
+    "url": "/manage-task/index.html"
+  }
+];
